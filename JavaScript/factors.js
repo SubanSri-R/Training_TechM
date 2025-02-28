ba36846e2fb4e47fcfcdacad5dd6f1d15559f05e
SubanSri-R <146161436+SubanSri-R@users.noreply.github.com>
2025-02-28 08:38:39 +0530

    Delete JavaScript directory

--- JavaScript/factors.js
+++
@@ -1,8 +0,0 @@
-function fact(num){
-    for (var i=1; i<=num; i++){
-        if (num%i==0){
-            console.log(i);
-        }
-    }
-}
-fact(10);
--- JavaScript/mul.js
+++
@@ -1,9 +0,0 @@
-var num=5
-var lim=10
-function multi(){
-    for (var i=1; i<=lim; i++)
-    {
-console.log(`${i} * ${num} = ${num*i}`)
-    }
-}
-multi();
